868c43aaedb2769bb2a87bb6e9134b8b56bc9e6c
Alex Faber <alexfaber2011@gmail.com>
2015-09-16 12:27:31 -0500

    fix - no need to doall when finding cells to toggle

--- src/exercises/core.clj
+++ src/exercises/core.clj
@@ -7,6 +7,33 @@
                   [0 1 0 0 0]
                   [1 0 0 0 1]])
 
+(defn parse-lines
+  "takes a vector of strings of numbers and returns a 2d vector containing only integers"
+  [vec-of-strings]
+  (vec (for [line vec-of-strings]
+         (vec (for [char line]
+                (-> (str char)
+                    Integer/parseInt))))))
+
+(defn parse-input
+  "takes repl input in the form of:
+
+    01000
+    10011
+    11001
+    01000
+    10001
+
+   and turns it into a vector of vectors of integers"
+  [input]
+  (try
+    (let [parsed-lines (->> input
+                            clojure.string/split-lines
+                            (mapv clojure.string/trim)
+                            parse-lines)]
+      [parsed-lines nil])
+    (catch Exception e [nil e])))
+
 (defn get-neighbors
   "returns a vector of neighbors (left, down, right, up), nil prunning where necessary"
   [matrix row col]
@@ -40,40 +67,15 @@
   "takes a matrix (vector of vectors of integers) and spits out all the coordinates of the cells to toggle"
   (let [height (count matrix)
         width (count (get matrix 0))]
-    (->> (doall (for [row (range height)]
-                  (doall (for [col (range width)]
-                           (when (toggle-cell? matrix row col)
-                             [row col])))))
+    (->> (for [row (range height)]
+           (for [col (range width)]
+                     (when (toggle-cell? matrix row col)
+                       [row col])))
          flatten
          (remove nil?)
          (partition 2))))
 
-(defn parse-lines
-  "takes a vector of strings of numbers and returns a 2d vector containing only integers"
-  [vec-of-strings]
-  (vec (for [line vec-of-strings]
-         (vec (for [char line]
-                (-> (str char)
-                    Integer/parseInt))))))
-
-(defn parse-input
-  "takes repl input in the form of:
-
-    01000
-    10011
-    11001
-    01000
-    10001
-
-   and turns it into a vector of vectors of integers"
-  [input]
-  (try
-    (let [parsed-lines (->> input
-                            clojure.string/split-lines
-                            (mapv clojure.string/trim)
-                            parse-lines)]
-      [parsed-lines nil])
-    (catch Exception e [nil e])))
+()
 
 (defn evolve
   [input]
