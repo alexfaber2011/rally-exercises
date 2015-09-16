c50188797ec958394671e660c9468e1d1afd4a90
Alex Faber <alexfaber2011@gmail.com>
2015-09-15 19:20:49 -0500

    implement get-neighbors function

--- src/exercises/core.clj
+++ src/exercises/core.clj
@@ -1,3 +1,18 @@
 (ns exercises.core
   (:gen-class))
 
+(def test-matrix [[0 1 0 0 0]
+                  [1 0 0 1 1]
+                  [1 1 0 0 1]
+                  [0 1 0 0 0]
+                  [1 0 0 0 1]])
+
+(defn get-neighbors
+  "returns a list of neighbors (left, down, right, up), nil prunning where necessary"
+  [matrix row col]
+  (let [left (get-in matrix [row (dec col)])
+        down (get-in matrix [(dec row) col])
+        right (get-in matrix [row (inc col)])
+        up (get-in matrix [(inc row) col])]
+    (->> (list left down right up)
+         (remove nil?))))
