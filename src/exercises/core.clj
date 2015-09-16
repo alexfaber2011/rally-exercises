f634bf7eeb2469bd9410be6042cda52bedc52226
Alex Faber <alexfaber2011@gmail.com>
2015-09-15 19:29:15 -0500

    implement get-neighbor-count function

--- src/exercises/core.clj
+++ src/exercises/core.clj
@@ -8,11 +8,18 @@
                   [1 0 0 0 1]])
 
 (defn get-neighbors
-  "returns a list of neighbors (left, down, right, up), nil prunning where necessary"
+  "returns a vector of neighbors (left, down, right, up), nil prunning where necessary"
   [matrix row col]
   (let [left (get-in matrix [row (dec col)])
         down (get-in matrix [(dec row) col])
         right (get-in matrix [row (inc col)])
         up (get-in matrix [(inc row) col])]
     (->> (list left down right up)
-         (remove nil?))))
+         (remove nil?)
+         vec)))
+
+(defn get-neighbor-count
+  "retuns an integer representing the number of 1's in the neighborhood"
+  [neighbors]
+  (reduce + neighbors))
+
